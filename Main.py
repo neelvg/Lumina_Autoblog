7ca477891d3bd2be31115d354574b68bc8337f5c
Neel Gandhi <138033072+neelvg@users.noreply.github.com>
2023-07-16 18:44:59 +0530

    Update and rename Main.js to Main.py

---
+++ Main.py
@@ -0,0 +1,111 @@
+MAX_POSTS_PER_DAY = 5
+
+def research():
+  # This function researches high-demand blog titles, competitors, analysis, SEO, and high keywords for blogger posts.
+  # It then transfers the details to another function.
+
+  # Get the list of high-demand blog titles.
+  high_demand_titles = get_high_demand_titles()
+
+  # Get the list of competitors for each high-demand title.
+  competitors = get_competitors(high_demand_titles)
+
+  # Analyze the competitors' SEO and keywords.
+  seo = analyze_seo(competitors)
+  keywords = analyze_keywords(competitors)
+
+  # Transfer the details to another function.
+  transfer_details(high_demand_titles, seo, keywords)
+
+def generate_titles():
+  # This function generates blog titles according to the researches and keywords.
+
+  # Get the list of high-demand titles from the previous function.
+  high_demand_titles = get_details("highDemandTitles")
+
+  # Get the list of SEO and keywords from the previous function.
+  seo = get_details("seo")
+  keywords = get_details("keywords")
+
+  # Generate blog titles based on the high-demand titles, SEO, and keywords.
+  titles = generate_titles(high_demand_titles, seo, keywords)
+
+  # Return the list of titles.
+  return titles
+
+def generate_contents():
+  # This function generates blogspot post contents on the generated blog titles.
+
+  # Get the list of titles from the previous function.
+  titles = get_details("titles")
+
+  # Generate blogspot post contents based on the titles.
+  contents = generate_contents(titles)
+
+  # Return the list of contents.
+  return contents
+
+def rewrite():
+  # This function rewrites the article by anti-plagiarism, grammatically, and paraphrasings, etc.
+
+  # Get the list of contents from the previous function.
+  contents = get_details("contents")
+
+  # Rewrite the articles.
+  rewritten_contents = await bard.rewrite(contents)
+
+  # Return the list of rewritten contents.
+  return rewritten_contents
+
+def publish():
+  # This function creates a new post on Blogger and fills the keywords, titles, and blog contents.
+
+  # Get the list of titles from the previous function.
+  titles = get_details("titles")
+
+  # Get the list of rewritten contents from the previous function.
+  contents = get_details("rewrittenContents")
+
+  # Create a new post on Blogger.
+  create_post(titles, contents)
+
+def main():
+  # This function is the main function.
+  # It calls the research(), generateTitles(), generateContents(), rewrite(), and publish() functions.
+
+  research()
+
+  # Set the number of posts to create per day.
+  num_posts = MAX_POSTS_PER_DAY
+
+  # Loop through the number of posts to create.
+  for i in range(num_posts):
+    # Generate the titles, contents, and rewritten contents.
+    titles = generate_titles()
+    contents = generate_contents()
+    rewritten_contents = rewrite(contents)
+
+    # Check if the title is already taken.
+    is_title_taken = check_title(titles[i])
+
+    # If the title is not taken, create a new post on Blogger.
+    if not is_title_taken:
+      publish(titles[i], rewritten_contents)
+
+  # Set the Blogger blog's URL.
+  blogger_url = "https://www.my-blogger-blog.com"
+
+  # Set the Blogger blog's username and password.
+  blogger_username = "my-username"
+  blogger_password = "my-password"
+
+  # Create a new Blogger client.
+  client = Blogger({
+    blogger_url,
+    blogger_username,
+    blogger_password,
+  })
+
+  # Publish the posts to Blogger.
+  client.publish_posts(titles, rewritten_contents)
+    
